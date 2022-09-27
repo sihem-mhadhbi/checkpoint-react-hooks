f9aaaa0a399c6cac48f72fe6eab20f57a0657d42
sihem <mhadhbisihem0@gmail.com>
2022-09-27 19:48:24 +0200

    create react hooks

--- src/Components/AddMovie.js
+++ src/Components/AddMovie.js
@@ -39,18 +39,31 @@ const AddMovie = (props) => {
       <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
         <h1 style={{ color: "black" }}>Add Movie</h1>
         <form>
+          <h5 style={{ color: "black" }}>Title:</h5>
           <input
             type="text"
             name="title"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
+          <h5 style={{ color: "black" }}>PosterURL:</h5>
           <input
             type="url"
             name="posterURL"
             value={posterURL}
             onChange={(e) => setPosterURL(e.target.value)}
+            style={{ color: "#556B2F", border: "solid 1px #556B2F" }}
           />
+          <h5 style={{ color: "black" }}>Description:</h5>
+          <textarea
+            name="description"
+            cols="30"
+            rows="10"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            style={{ color: "#556B2F", border: "solid 1px #556B2F" }}
+          ></textarea>
+          <h5 style={{ color: "black" }}>Rating:</h5>
           <input
             type="number"
             name="rating"
@@ -58,17 +71,31 @@ const AddMovie = (props) => {
             max="5"
             value={rating}
             onChange={(e) => setRating(e.target.value)}
+            style={{ color: "#556B2F", border: "solid 1px #556B2F" }}
           />
-          <textarea
-            name="description"
-            cols="30"
-            rows="10"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          ></textarea>
         </form>
-        <button onClick={handleAdd}>add</button>
-        <button onClick={closeModal}>close</button>
+        <button
+          style={{
+            color: "blue",
+            margin: "7px",
+            border: "solid 1px blue",
+            padding: "1rem",
+          }}
+          onClick={handleAdd}
+        >
+          add
+        </button>
+        <button
+          style={{
+            color: "blue",
+            margin: "7px",
+            padding: "1rem",
+            border: "solid 1px blue",
+          }}
+          onClick={closeModal}
+        >
+          close
+        </button>
       </Modal>
     </div>
   );
